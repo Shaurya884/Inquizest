10f3799e1afc3810ce43668eb4335346fc37e6a6
SVR7 <101692719+Shaurya884@users.noreply.github.com>
2026-09-08 21:26:57 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,12 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   tanstackStart: {
-    server: { entry: "server" },
+    server: {
+      entry: "server",
+    },
+
+    spa: {
+      enabled: true,
+    },
   },
 });
